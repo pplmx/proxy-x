2141d71b3278b7933ab2e9ba328a4e0a392d63df
Mystic <215104920@qq.com>
2023-12-22 13:54:30 +0800

    refactor: use clap derive

--- src/cli.rs
+++ src/cli.rs
@@ -1,39 +1,38 @@
-use clap::{arg, command, ArgMatches, Command};
+use clap::{arg, command, Args, Parser, Subcommand};
 
 use proxy_x::{disable_proxy, enable_proxy, get_agent_ip};
 
+#[derive(Parser)]
+#[command(arg_required_else_help = true, author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Get the current IP address")]
+    Ip,
+    #[command(about = "Enable proxy")]
+    Enable(EnableArgs),
+    Disable, // if no about is provided, #[command(about = "")] can be omitted
+}
+
+#[derive(Args)]
+struct EnableArgs {
+    #[arg(required = true)]
+    proxy_url: String,
+}
+
 pub fn execute() {
-    let matches = parser();
-    handler(matches);
-}
-
-fn parser() -> ArgMatches {
-    command!() // requires "cargo" feature in clap
-        .arg_required_else_help(true)
-        .subcommands(vec![
-            Command::new("ip").about("Access the agent's IP address"),
-            Command::new("enable")
-                .about("Enables the proxy")
-                .arg(arg!(<PROXY_URL> "e.g. http://localhost:7890")),
-            Command::new("disable").about("Disables the proxy"),
-        ])
-        .get_matches()
-}
-
-fn handler(matches: ArgMatches) {
-    match matches.subcommand_name() {
-        Some("enable") => {
-            let sub_matches = matches.subcommand_matches("enable").unwrap();
-            let proxy_url = sub_matches.get_one::<String>("PROXY_URL").unwrap();
-            enable_proxy(proxy_url);
-        }
-        Some("disable") => {
-            disable_proxy();
-        }
-        Some("ip") => match get_agent_ip() {
+    let cli = Cli::parse();
+    match cli.command {
+        Some(Commands::Ip) => match get_agent_ip() {
             Ok(ip) => println!("Current IP: {}", ip),
             Err(e) => eprintln!("Error getting IP: {}", e),
         },
-        _ => {}
+        Some(Commands::Enable(args)) => enable_proxy(&args.proxy_url),
+        Some(Commands::Disable) => disable_proxy(),
+        None => {}
     }
 }
